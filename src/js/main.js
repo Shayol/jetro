c987e0794ab4cc2165f2bd9b58c55886d8429eb4
Shayol <vorga88@gmail.com>
2018-07-17 12:11:06 +0300

    add pagination

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function (e) {
     var filtersParent = document.querySelector('.portfolio__filter');
     var filters = document.querySelectorAll('.portfolio__filter-link');
     var works = document.querySelectorAll('.recent-works__item');
+    var posts = document.querySelectorAll('.post');
+    const perPage = 5;
 
     var navLinks = document.querySelectorAll('.nav__link');
 
@@ -47,6 +49,51 @@ window.addEventListener('load', function (e) {
             link.classList.remove('nav__link--active');
         }
     });
+
+    if (posts) {
+        var pagination = document.querySelector('.blog__pagination');
+        var pagesNum = Math.ceil(posts.length / perPage);
+        pagination.innerHTML = '';
+
+        for (var i = 0; i < pagesNum; i++) {
+            pagination.innerHTML += '<div class="pagination__button">' + (i + 1) + '</div>';
+        }
+        var pages = pagination.querySelectorAll('.pagination__button');
+
+        if (pages.length > 1) {
+            pages[0].classList.add('pagination__active');
+        }
+
+        for (let k = 0; k < perPage; k++) {
+            if(posts[k]) {
+                posts[k].style.display = 'block';
+            }
+        }
+
+        pages.forEach((page, n) => {
+
+            page.addEventListener('click', (e) => {
+
+                var number = parseInt(e.target.innerText);
+
+                var from = perPage * number - perPage;
+                var to = perPage * number - 1;
+                posts.forEach((post, i) => {
+                    if (i >= from && i <= to) {
+                        post.style.display = 'block';
+                    }
+                    else {
+                        post.style.display = 'none';
+                    }
+                });
+                pages.forEach(el => el.classList.remove('pagination__active'));
+                e.target.classList.add('pagination__active');
+
+            });
+        });
+
+
+    }
 
 
 });
